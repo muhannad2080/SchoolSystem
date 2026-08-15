3f8fa8ccb320d159e88407f81c3eba3911a4bfd4
muhannad2080 <2.48511242e+08+muhannad2080@users.noreply.github.com>
2026-08-15 19:17:30 +0000

    Normalize existing section data

---
+++ Databass/Migration_NormalizeExistingSections.sql
@@ -0,0 +1,108 @@
+/*
+    توحيد بيانات الشعب الموجودة مسبقاً.
+    يحول القيم المختصرة أو ذات المسافات إلى الأسماء القياسية:
+    ألف، باء، جيم، دال.
+    السكربت آمن لإعادة التنفيذ ولا ينشئ سجلات جديدة.
+*/
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    IF OBJECT_ID(N'dbo.StudentClasses', N'U') IS NOT NULL
+    BEGIN
+        UPDATE dbo.StudentClasses
+        SET Section = CASE LTRIM(RTRIM(Section))
+                          WHEN N'أ' THEN N'ألف'
+                          WHEN N'ا' THEN N'ألف'
+                          WHEN N'ألف' THEN N'ألف'
+                          WHEN N'ب' THEN N'باء'
+                          WHEN N'باء' THEN N'باء'
+                          WHEN N'ج' THEN N'جيم'
+                          WHEN N'جيم' THEN N'جيم'
+                          WHEN N'د' THEN N'دال'
+                          WHEN N'دال' THEN N'دال'
+                          ELSE NULLIF(LTRIM(RTRIM(Section)), N'')
+                       END
+        WHERE Section IS NOT NULL;
+    END;
+
+    IF OBJECT_ID(N'dbo.StudentAttendance', N'U') IS NOT NULL
+    BEGIN
+        UPDATE dbo.StudentAttendance
+        SET Section = CASE LTRIM(RTRIM(Section))
+                          WHEN N'أ' THEN N'ألف'
+                          WHEN N'ا' THEN N'ألف'
+                          WHEN N'ب' THEN N'باء'
+                          WHEN N'ج' THEN N'جيم'
+                          WHEN N'د' THEN N'دال'
+                          ELSE NULLIF(LTRIM(RTRIM(Section)), N'')
+                      END
+        WHERE Section IS NOT NULL;
+    END;
+
+    IF OBJECT_ID(N'dbo.Enrollments', N'U') IS NOT NULL
+    BEGIN
+        UPDATE dbo.Enrollments
+        SET Section = CASE LTRIM(RTRIM(Section))
+                          WHEN N'أ' THEN N'ألف'
+                          WHEN N'ا' THEN N'ألف'
+                          WHEN N'ب' THEN N'باء'
+                          WHEN N'ج' THEN N'جيم'
+                          WHEN N'د' THEN N'دال'
+                          ELSE NULLIF(LTRIM(RTRIM(Section)), N'')
+                      END
+        WHERE Section IS NOT NULL;
+    END;
+
+    IF OBJECT_ID(N'dbo.SchoolTimetable', N'U') IS NOT NULL
+    BEGIN
+        UPDATE dbo.SchoolTimetable
+        SET Section = CASE LTRIM(RTRIM(Section))
+                          WHEN N'أ' THEN N'ألف'
+                          WHEN N'ا' THEN N'ألف'
+                          WHEN N'ب' THEN N'باء'
+                          WHEN N'ج' THEN N'جيم'
+                          WHEN N'د' THEN N'دال'
+                          ELSE NULLIF(LTRIM(RTRIM(Section)), N'')
+                      END
+        WHERE Section IS NOT NULL;
+    END;
+
+    IF COL_LENGTH(N'dbo.Students', N'Section') IS NOT NULL
+    BEGIN
+        UPDATE dbo.Students
+        SET Section = CASE LTRIM(RTRIM(Section))
+                          WHEN N'أ' THEN N'ألف'
+                          WHEN N'ا' THEN N'ألف'
+                          WHEN N'ب' THEN N'باء'
+                          WHEN N'ج' THEN N'جيم'
+                          WHEN N'د' THEN N'دال'
+                          ELSE NULLIF(LTRIM(RTRIM(Section)), N'')
+                      END
+        WHERE Section IS NOT NULL;
+    END;
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    IF XACT_STATE() <> 0 ROLLBACK TRANSACTION;
+    THROW;
+END CATCH;
+
+SELECT N'StudentClasses' AS TableName, Section, COUNT(*) AS RowCount
+FROM dbo.StudentClasses
+WHERE Section IS NOT NULL
+GROUP BY Section
+UNION ALL
+SELECT N'StudentAttendance', Section, COUNT(*)
+FROM dbo.StudentAttendance
+WHERE Section IS NOT NULL
+GROUP BY Section
+UNION ALL
+SELECT N'SchoolTimetable', Section, COUNT(*)
+FROM dbo.SchoolTimetable
+WHERE Section IS NOT NULL
+GROUP BY Section;
+GO
