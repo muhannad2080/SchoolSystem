8f6cec29e2559385a5f9856d0f822cefcdc8a313
muhannad2080 <2.48511242e+08+muhannad2080@users.noreply.github.com>
2026-08-16 12:59:36 +0000

    Fix responsive RTL welcome screen layout

--- UI/WelcomeScreen.Designer.cs
+++ UI/WelcomeScreen.Designer.cs
@@ -50,32 +50,35 @@
             this.panelCenter.Controls.Add(this.lblSystemName);
             this.panelCenter.Controls.Add(this.lblTagline);
             this.panelCenter.Controls.Add(this.flowQuickLinks);
-            this.panelCenter.Location = new System.Drawing.Point(175, 80);
+            this.panelCenter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelCenter.Location = new System.Drawing.Point(220, 100);
             this.panelCenter.Name = "panelCenter";
-            this.panelCenter.Size = new System.Drawing.Size(600, 400);
+            this.panelCenter.Size = new System.Drawing.Size(760, 360);
             this.panelCenter.TabIndex = 0;
 
             // 
             // lblIcon
             // 
-            this.lblIcon.AutoSize = true;
-            this.lblIcon.Font = new System.Drawing.Font("Tahoma", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIcon.AutoSize = false;
+            this.lblIcon.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblIcon.Font = new System.Drawing.Font("Tahoma", 54F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblIcon.ForeColor = System.Drawing.Color.White;
-            this.lblIcon.Location = new System.Drawing.Point(230, 20);
+            this.lblIcon.Location = new System.Drawing.Point(0, 0);
             this.lblIcon.Name = "lblIcon";
-            this.lblIcon.Size = new System.Drawing.Size(108, 116);
+            this.lblIcon.Size = new System.Drawing.Size(760, 92);
             this.lblIcon.TabIndex = 0;
             this.lblIcon.Text = "🏫";
 
             // 
             // lblSystemName
             // 
-            this.lblSystemName.AutoSize = true;
-            this.lblSystemName.Font = new System.Drawing.Font("Tahoma", 26F, System.Drawing.FontStyle.Bold);
+            this.lblSystemName.AutoSize = false;
+            this.lblSystemName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblSystemName.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold);
             this.lblSystemName.ForeColor = System.Drawing.Color.White;
-            this.lblSystemName.Location = new System.Drawing.Point(150, 180);
+            this.lblSystemName.Location = new System.Drawing.Point(0, 92);
             this.lblSystemName.Name = "lblSystemName";
-            this.lblSystemName.Size = new System.Drawing.Size(300, 42);
+            this.lblSystemName.Size = new System.Drawing.Size(760, 56);
             this.lblSystemName.TabIndex = 1;
             this.lblSystemName.Text = "نظام إدارة المدرسة";
             this.lblSystemName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -83,12 +86,13 @@
             // 
             // lblTagline
             // 
-            this.lblTagline.AutoSize = true;
+            this.lblTagline.AutoSize = false;
+            this.lblTagline.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblTagline.Font = new System.Drawing.Font("Tahoma", 12F);
             this.lblTagline.ForeColor = System.Drawing.Color.LightGray;
-            this.lblTagline.Location = new System.Drawing.Point(140, 230);
+            this.lblTagline.Location = new System.Drawing.Point(0, 148);
             this.lblTagline.Name = "lblTagline";
-            this.lblTagline.Size = new System.Drawing.Size(320, 19);
+            this.lblTagline.Size = new System.Drawing.Size(760, 34);
             this.lblTagline.TabIndex = 2;
             this.lblTagline.Text = "الحل المتكامل لإدارة المؤسسات التعليمية";
             this.lblTagline.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -100,9 +104,14 @@
             this.flowQuickLinks.Controls.Add(this.btnTeachers);
             this.flowQuickLinks.Controls.Add(this.btnFinance);
             this.flowQuickLinks.Controls.Add(this.btnAttendance);
-            this.flowQuickLinks.Location = new System.Drawing.Point(30, 320);
+            this.flowQuickLinks.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowQuickLinks.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowQuickLinks.Location = new System.Drawing.Point(0, 302);
             this.flowQuickLinks.Name = "flowQuickLinks";
-            this.flowQuickLinks.Size = new System.Drawing.Size(540, 60);
+            this.flowQuickLinks.Padding = new System.Windows.Forms.Padding(0, 6, 0, 6);
+            this.flowQuickLinks.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.flowQuickLinks.Size = new System.Drawing.Size(760, 58);
+            this.flowQuickLinks.WrapContents = false;
             this.flowQuickLinks.TabIndex = 3;
 
             // 
@@ -115,7 +124,8 @@
             this.btnStudents.ForeColor = System.Drawing.Color.White;
             this.btnStudents.Location = new System.Drawing.Point(3, 10);
             this.btnStudents.Name = "btnStudents";
-            this.btnStudents.Size = new System.Drawing.Size(120, 40);
+            this.btnStudents.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.btnStudents.Size = new System.Drawing.Size(164, 44);
             this.btnStudents.TabIndex = 0;
             this.btnStudents.Text = "👨‍🎓 الطلاب";
             this.btnStudents.UseVisualStyleBackColor = false;
@@ -131,7 +141,8 @@
             this.btnTeachers.ForeColor = System.Drawing.Color.White;
             this.btnTeachers.Location = new System.Drawing.Point(129, 10);
             this.btnTeachers.Name = "btnTeachers";
-            this.btnTeachers.Size = new System.Drawing.Size(120, 40);
+            this.btnTeachers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.btnTeachers.Size = new System.Drawing.Size(164, 44);
             this.btnTeachers.TabIndex = 1;
             this.btnTeachers.Text = "👨‍🏫 المعلمين";
             this.btnTeachers.UseVisualStyleBackColor = false;
@@ -147,7 +158,8 @@
             this.btnFinance.ForeColor = System.Drawing.Color.White;
             this.btnFinance.Location = new System.Drawing.Point(255, 10);
             this.btnFinance.Name = "btnFinance";
-            this.btnFinance.Size = new System.Drawing.Size(120, 40);
+            this.btnFinance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.btnFinance.Size = new System.Drawing.Size(164, 44);
             this.btnFinance.TabIndex = 2;
             this.btnFinance.Text = "💰 المالية";
             this.btnFinance.UseVisualStyleBackColor = false;
@@ -163,7 +175,8 @@
             this.btnAttendance.ForeColor = System.Drawing.Color.White;
             this.btnAttendance.Location = new System.Drawing.Point(381, 10);
             this.btnAttendance.Name = "btnAttendance";
-            this.btnAttendance.Size = new System.Drawing.Size(120, 40);
+            this.btnAttendance.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.btnAttendance.Size = new System.Drawing.Size(164, 44);
             this.btnAttendance.TabIndex = 3;
             this.btnAttendance.Text = "📝 الحضور";
             this.btnAttendance.UseVisualStyleBackColor = false;
--- UI/WelcomeScreen.cs
+++ UI/WelcomeScreen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using SchoolSystem.UI; // لاحظ أننا نستخدم نفس الـ namespace
 
@@ -17,6 +18,66 @@ namespace SchoolSystem.UI
             InitializeComponent();
             SchoolSystem.Helpers.UIHelper.ApplyStyle(this);
             this.Dock = DockStyle.Fill;
+            ConfigureResponsiveLayout();
+            Resize += (sender, e) => CenterContent();
+            CenterContent();
+        }
+
+        private void ConfigureResponsiveLayout()
+        {
+            RightToLeft = RightToLeft.Yes;
+            panelBackground.Dock = DockStyle.Fill;
+            panelCenter.Anchor = AnchorStyles.None;
+            panelCenter.Size = new Size(760, 360);
+            panelCenter.BackColor = Color.Transparent;
+
+            lblIcon.AutoSize = false;
+            lblIcon.Dock = DockStyle.Top;
+            lblIcon.Height = 92;
+            lblIcon.Margin = Padding.Empty;
+            lblIcon.TextAlign = ContentAlignment.MiddleCenter;
+
+            lblSystemName.AutoSize = false;
+            lblSystemName.Dock = DockStyle.Top;
+            lblSystemName.Height = 56;
+            lblSystemName.Margin = Padding.Empty;
+            lblSystemName.TextAlign = ContentAlignment.MiddleCenter;
+
+            lblTagline.AutoSize = false;
+            lblTagline.Dock = DockStyle.Top;
+            lblTagline.Height = 34;
+            lblTagline.Margin = Padding.Empty;
+            lblTagline.TextAlign = ContentAlignment.MiddleCenter;
+
+            flowQuickLinks.Dock = DockStyle.Bottom;
+            flowQuickLinks.Height = 58;
+            flowQuickLinks.Width = panelCenter.ClientSize.Width;
+            flowQuickLinks.Margin = Padding.Empty;
+            flowQuickLinks.Padding = new Padding(0, 6, 0, 6);
+            flowQuickLinks.FlowDirection = FlowDirection.RightToLeft;
+            flowQuickLinks.WrapContents = false;
+            flowQuickLinks.AutoScroll = false;
+            flowQuickLinks.RightToLeft = RightToLeft.Yes;
+
+            Button[] buttons = { btnStudents, btnTeachers, btnFinance, btnAttendance };
+            foreach (Button button in buttons)
+            {
+                button.AutoSize = false;
+                button.Size = new Size(164, 44);
+                button.Margin = new Padding(4, 0, 4, 0);
+                button.Anchor = AnchorStyles.None;
+                button.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void CenterContent()
+        {
+            if (panelBackground == null || panelCenter == null)
+                return;
+
+            panelCenter.Left = Math.Max(0, (panelBackground.ClientSize.Width - panelCenter.Width) / 2);
+            panelCenter.Top = Math.Max(0, (panelBackground.ClientSize.Height - panelCenter.Height) / 2);
+            flowQuickLinks.Width = panelCenter.ClientSize.Width;
         }
 
         private void btnStudents_Click(object sender, EventArgs e)
